4614fe9c70083ef5f6e1f06c8b19795d264607db
leovieirad <leovieiradsz@gmail.com>
2024-10-01 20:13:52 -0300

    Criação do model Variação

--- produto/models.py
+++ produto/models.py
@@ -1,4 +1,7 @@
+from PIL import Image
 from django.db import models
+import os
+from django.conf import settings
 
 class Produto(models.Model):
     nome = models.CharField(max_length=255)
@@ -17,6 +20,38 @@ class Produto(models.Model):
         )
     )
 
+
+
+    @staticmethod
+    def resize_image(img, new_width=800):
+        img_full_path = os.path.join(settings.MEDIA_ROOT, img.name)
+        img_pil = Image.open(img_full_path)
+        original_width, original_height = img_pil.size
+
+        if original_width >= new_width:
+            img_pil.close
+            return
+        
+        new_height = round((new_width * original_height) / original_width)
+
+        new_img = img_pil.resize((new_width, new_height), Image.LANCZOS)
+        new_img.save(
+            img_full_path,
+            optimize=True,
+            quality=50
+        )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        max_image_size = 800
+
+        if self.imagem:
+            self.resize_image(self.imagem, max_image_size)
+
+    def __str__(self):
+        return self.nome
+
 '''
         Variacao:
             nome - char
@@ -26,3 +61,16 @@ class Produto(models.Model):
             estoque - Int
 
 '''
+class Variacao(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=50, blank=True, null=True)
+    preco = models.FloatField()
+    preco_promocional = models.FloatField(default=0)
+    estoque = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.nome or self.produto.nome
+
+    class Meta:
+        verbose_name = 'Variação'
+        verbose_name_plural = 'Variações'
