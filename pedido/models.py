17b71872ce3e0f0c23a77b59f3410bbd0d40f24f
leovieirad <leovieiradsz@gmail.com>
2024-11-06 20:24:58 -0300

    Salvando o pedido

--- pedido/models.py
+++ pedido/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Pedido(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     total = models.FloatField()
+    qtd_total = models.PositiveIntegerField(default=0)
     status = models.CharField(
         default = "C",
         max_length = 1,
@@ -37,3 +38,4 @@ class ItemPedido(models.Model):
     class Meta:
         verbose_name = "Item do pedido"
         verbose_name_plural = "Itens do pedido"
+        
